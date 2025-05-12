2e1198b4d11879a01428568c956e21b04231729c
CHINEDU UZORUE <53054791+CHINEDU-UZORUE@users.noreply.github.com>
2025-05-12 17:23:37 +0100

    Create config_template.py

---
+++ config_template.py
@@ -0,0 +1,13 @@
+# config.py
+
+# X API Credentials
+BEARER_TOKEN = "Yourbearertokenhere"
+
+# Birdeye API Key
+BIRDEYE_API_KEY = "YourBirdeyeAPIkeyhere"
+
+# Groq API Key
+GROQ_API_KEY = "YourGroqAPIkeyhere"
+
+
+
